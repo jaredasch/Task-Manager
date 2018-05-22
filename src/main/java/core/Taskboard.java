93d738960a15269060b5307f21b05bc8b2cf0548
jaredasch1 <jaredasch1@gmail.com>
2018-05-22 02:10:41 -0400

    Implement Task serialization

---
+++ src/main/java/core/Taskboard.java
@@ -0,0 +1,13 @@
+package main.java.core;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jaredasch on 5/22/18.
+ */
+public class Taskboard {
+    private String name;
+    private String description;
+
+    private ArrayList<Task> tasks;
+}
